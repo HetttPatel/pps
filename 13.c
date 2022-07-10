bb602042f7c88b10e7e21af9913fe37226680bb8
Het Patel <96485370+HetttPatel@users.noreply.github.com>
2022-07-10 12:51:38 +0530

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,23 @@
+//program to find the factorial of a given number
+#include<stdio.h>
+int main(){
+
+    int t=1;
+    int a;
+    unsigned int factorial;
+    
+    printf("Enter an Integer: ");
+    scanf("%d",&a);
+
+    if(a>0){
+
+    while(t!=a){
+        factorial = a*t;
+        t++;
+    }   
+       printf("Factorial is %d\n",factorial); 
+    }
+
+
+    return 0;
+}
